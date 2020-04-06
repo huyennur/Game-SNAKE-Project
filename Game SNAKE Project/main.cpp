a08f3ffece54a026e65e9e0dbabd348651319895
Ngoc Huyen <huyen111@users.noreply.github.com>
2020-04-06 16:42:13 +0700

    Initial Commit

---
+++ Game SNAKE Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game SNAKE Project
+//
+//  Created by DO NGOC HUYEN on 4/6/20.
+//  Copyright © 2020 DO NGOC HUYEN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
